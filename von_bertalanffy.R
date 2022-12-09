0039510555db68f50e5f15cc04faa3cb3e2e65e2
AJSRochaa <alberto.jmas.rocha@gmail.com>
2022-12-09 14:46:45 +0000

    funcao melhorada; falta perfis

--- von_bertalanffy.R
+++ von_bertalanffy.R
@@ -5,10 +5,25 @@ library(ggplot2)
 
 df = read.csv('leituras.csv')
 
+names(df)
+
+for(i in seq(0,10,0.1)){
+  line = c(NA,NA,i,"Jan",'S1',0)
+  df = rbind(df, line)
+}; rm(i,line)
+
+df = df %>% mutate(age = as.numeric(age),
+                   Lt = as.numeric(Lt))
+
 vb = vbFuns(param = 'Typical')
 
 f.starts = vbStarts(Lt ~ age, data = df)
+f.starts$Linf = 30; f.starts$K = 1; f.starts$t0 = 0
 
 f.fit = nls(Lt ~ vb(age, Linf, K, t0), data = df, start = f.starts)
 
 f.fit
+
+df %>%
+ggplot() +
+  geom_point(aes(x = age, y = Lt))
